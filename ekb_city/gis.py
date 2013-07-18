9e3f438238f814d8571bc0cf42afb3da3eac4713
Messiah <m.muzafarov@gmail.com>
2013-07-18 12:08:00 +0600

    matches

--- ekb_city/gis.py
+++ ekb_city/gis.py
@@ -10,16 +10,36 @@ class Finder(object):
 
     def DB(self):
         """Load 2Gis.csv file"""
-        pass
+        database = []
+        try:
+            database = open("2Gis.csv").readlines()
+            database = map(lambda z: (u"{}, {}".format(z[1].strip(),
+                                                       z[0].strip()),
+                                      z[2].strip()),
+                           map(lambda y: y.split(";"),
+                           map(lambda x: x.decode("utf-8"), database)))
+        except IOError:
+            print("[ERROR]: File 2Gis.csv not found")
+        return database
 
     def Streets(self):
         """Aggregate addresses into dictionary by streets"""
         streets = dict()
+        for street, house in self.database:
+            if street in streets:
+                streets[street].append(house)
+            else:
+                streets[street] = [house, ]
         return streets
 
     def Houses(self):
         """Aggregate addresses into dictionary by houses"""
         houses = dict()
+        for street, house in self.database:
+            if house in houses:
+                houses[house].append(street)
+            else:
+                houses[house] = [street, ]
         return houses
 
     def match(self, address):
@@ -36,15 +56,26 @@ class Finder(object):
 
         return filter(lambda x: len(x) > 0, matches)
 
-    def matchHouse(self):
+    def matchHouse(self, address):
         """Return all matches by house number"""
         matches = []
+        try:
+            matches.extend(self.houses[address])
+        except KeyError:
+            for house in self.houses:
+                if address in house:
+                    matches.extend(self.houses[house])
         return matches
 
-    def matchStreet(self):
+    def matchStreet(self, address):
         """Return all matches by street name"""
         matches = []
+        try:
+            matches.extend(self.streets[address])
+        except KeyError:
+            for street in self.streets:
+                if address in street:
+                    matches.extend(self.streets[street])
         return matches
-
 
     #TODO: What if Street and House are known together?
