6ce6b9720f30bd43c62e76ad9afa6b272172d03f
Messiah <m.muzafarov@gmail.com>
2013-07-22 11:45:24 +0600

    .

--- ekb-city/csv2py.py
+++ ekb-city/csv2py.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-with open("database.py","w") as f:
+with open("database.py", "w") as f:
     f.write(u"\"\"\"2 Gis converted database of addresses\"\"\"\n")
     f.write(u"DATABASE = [\n")
     for address in open("2Gis.csv").readlines():
